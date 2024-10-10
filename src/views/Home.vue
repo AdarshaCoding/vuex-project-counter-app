90bba4206fa2b80409c2c92243a177f7802d6b1d
AdarshaCoding <adarshapc4467@gmail.com>
2024-10-10 23:07:59 +0530

    Vuex - counter app

---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <div class="counter-container">
+      <div class="counter">
+        <h1>{{ counter }}</h1>
+      </div>
+      <div class="buttons">
+        <button @click="decrementCounter">-</button>
+        <button @click="incrementCounter">+</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    decrementCounter() {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+    },
+    incrementCounter() {
+      this.counter++;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.counter-container {
+  border: 1px solid black;
+  width: 300px;
+  height: 200px;
+  margin: auto;
+
+  .counter {
+    font-size: 25px;
+  }
+
+  .buttons {
+    button {
+      width: 100px;
+      padding: 2px;
+      font-weight: bold;
+      font-size: 30px;
+      margin: 10px;
+      cursor: pointer;
+    }
+  }
+}
+</style>
